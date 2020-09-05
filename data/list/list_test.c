54249c8b9ca09a2604c795ae3186359b4a60d5ee
Kevin Key <kevinjameskey@gmail.com>
2020-09-05 09:47:32 -0400

    Fixing the list test

--- data/list/list_test.c
+++ data/list/list_test.c
@@ -11,17 +11,14 @@ struct test_t
 void main(void)
 {
     struct list_t list;
-    struct list_t reverse;
     struct test_t pool[16];
 
     list_init(&list);
-    list_init(&reverse);
 
     for(uint8_t i = 0u; i < 16; i++)
     {
         pool[i].data = i;
         list_add(&list, (struct list_item_t *)&(pool[i]), LIST_ADD_TAIL);
-        list_add(&reverse, (struct list_item_t *)&(pool[i]), LIST_ADD_HEAD);
     }
 
     LIST_LOOP_EACH(&list)
@@ -31,7 +28,15 @@ void main(void)
     }
     printf("\n");
 
-    LIST_LOOP_EACH(&reverse)
+    list_init(&list);
+
+    for(uint8_t i = 0u; i < 16; i++)
+    {
+        pool[i].data = i;
+        list_add(&list, (struct list_item_t *)&(pool[i]), LIST_ADD_HEAD);
+    }
+
+    LIST_LOOP_EACH(&list)
     {
         struct test_t * p_test = (struct test_t *)p_item;
         printf("%d...", p_test->data);
